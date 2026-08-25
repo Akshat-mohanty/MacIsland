45bb42a9d815c26455fb9b6646469b7e341f9743
Akshat Mohanty <aakshatmohanty@gmail.com>
2026-08-25 18:43:29 +0530

    Initial Commit

---
+++ MacIsland/MacIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacIslandApp.swift
+//  MacIsland
+//
+//  Created by Adrien Martin on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MacIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
